33d59b55fdd9a0ec2a61333ae6d5e760d36bb5ec
skaram13 <skaram13st@gmail.com>
2018-04-01 12:28:50 -0400

    Added inital navbar

---
+++ src/Shared/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import 
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                CONTACT COMING SOON!!!!!!!
+            </div>
+        )
+    }
+}
+
+export default Navbar;
