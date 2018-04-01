e96f9714ba94ac8a975694c907e8e56a081456f6
skaram13 <skaram13st@gmail.com>
2018-04-01 19:24:02 -0400

    Nav bar updates

--- src/Manifesto/Manifesto.js
+++ src/Manifesto/Manifesto.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import "./Manifesto.css";
+import Header from "../Shared/Header/Header";
 class Manifesto extends Component {
     constructor(props) {
         super(props);
@@ -9,7 +10,29 @@ class Manifesto extends Component {
     render() {
         return (
             <div>
-                Manifesto coming soon!!!
+                <Header/>
+                <div className="manifesto-container body-text">
+                <ul class="list-unstyled">
+                    <h1>AXIOMS</h1>
+                    <li>Architecture & art are expressions of the spiritual human condition;</li>
+                    <li>Architecture is spatial balance & spatial disruption;</li>
+                    <li>Architecture is the perception of the void & the sensation of the material;</li>
+                    <li>Architecture is equally psychological and physical;</li>
+                    <li>Architecture is also spiritual for uniting the two;</li>
+                    <li>Architecture & art are socio-political forces;</li>
+                    <li>Architecture & art should further political progress;</li>
+                    <li>Architecture & art histories are vital to cultural citizenship;</li>
+                    <li>Architecture & art must be made available to all members of societies;</li>
+                    <li>Architecture & art must be owned by the collective of all humanity.</li>
+
+                    <h1>PRINCIPLES</h1>
+                    <li>Museums and galleries must be free to ensure equal access to the arts.</li>
+                    <li>The Arts, Humanities, & Sciences must receive equal educational funding.</li>
+                    <li>Architecture & urbanism must be inclusive to all.</li>
+                    <li>Architecture & urbanism must responsibly respond to a globalized human community.</li>
+                    <li>Architecture must permit the full range of the human experience & emotion.</li>
+                </ul>
+                </div>
             </div>
         )
     }
---
+++ src/Shared/Header/Header.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {Nav, Navbar, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <Navbar className="nav-bar">
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to="/">ZACHARY TORRES</Link>
+                    </Navbar.Brand>
+                </Navbar.Header>
+                <Nav>
+                    <NavDropdown  eventKey={3} title="Manifesto" id="basic-nav-dropdown">
+                        <MenuItem eventKey={3.1}>Home</MenuItem>
+                        <MenuItem divider />
+                        <MenuItem eventKey={3.2}>Portfolio</MenuItem>
+                        <MenuItem divider />
+                        <MenuItem eventKey={3.3}>CV</MenuItem>
+                        <MenuItem divider />
+                        <MenuItem eventKey={3.4}>Manifesto</MenuItem>
+                        <MenuItem divider />
+                        <MenuItem eventKey={3.5}>Blog</MenuItem>
+                        <MenuItem divider />
+                        <MenuItem eventKey={3.6}>Contact</MenuItem>
+                    </NavDropdown>
+                </Nav>
+            </Navbar>
+        )
+    }
+}
+
+export default Header;
