8c1c572555225281a978387e4b07be6681525597
Samantha Karam <samantha.karam@libertymutual.com>
2018-04-09 20:02:39 -0400

    nothing

--- src/Main/Portfolio/Project/Project.js
+++ src/Main/Portfolio/Project/Project.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import "./Project.css";
-import {withRouter} from 'react-router';
-import Img from 'react-image';
 import {Carousel} from 'react-bootstrap';
 import pic11 from "../../../assets/Projects/DIA-BEACON/pic1.jpg";
 import pic12 from "../../../assets/Projects/DIA-BEACON/pic2.jpg";
@@ -14,15 +12,12 @@ class Project extends Component {
     render() {
         const slideItems = pics.map((pic, index) =>
             <Carousel.Item className="photo">
-                <Img alt="slide" key={index} src={pic} />
+                <img alt="slide" key={index} src={pic} />
             </Carousel.Item>
         );
         return(
             <div>
             <Header title="PROJECT"/>
-                <Carousel className="photos" carousel="true" slide>
-                    {slideItems}
-                </Carousel>
                 <h1 className="text">THIS IS A TEST BLAH BLAH BLAH</h1>
             </div>
         );
