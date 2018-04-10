4cba25ae1413d93cde71e096f908d43805373ee5
Samantha Karam <samantha.karam@libertymutual.com>
2018-04-10 15:48:40 -0400

    remove most images

--- src/Main/Portfolio/Project/Project.js
+++ src/Main/Portfolio/Project/Project.js
@@ -1,20 +1,10 @@
 import React, { Component } from "react";
 import "./Project.css";
-import {Carousel} from 'react-bootstrap';
-import pic11 from "../../../assets/Projects/DIA-BEACON/pic1.jpg";
-import pic12 from "../../../assets/Projects/DIA-BEACON/pic2.jpg";
-import pic13 from "../../../assets/Projects/DIA-BEACON/pic3.jpg";
 import Header from "../../Shared/Header/Header";
 
-const pics = [pic11,pic12,pic13];
 
 class Project extends Component {
     render() {
-        const slideItems = pics.map((pic, index) =>
-            <Carousel.Item className="photo">
-                <img alt="slide" key={index} src={pic} />
-            </Carousel.Item>
-        );
         return(
             <div>
             <Header title="PROJECT"/>
