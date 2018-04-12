ecb77afbf2346c6820734ca142f3eb5d1567fb1f
Samantha Karam <samantha.karam@libertymutual.com>
2018-04-12 13:57:39 -0400

    Revert "nothing"
    This reverts commit 8c1c572555225281a978387e4b07be6681525597.

--- src/Main/Portfolio/Project/Project.js
+++ src/Main/Portfolio/Project/Project.js
@@ -1,27 +1,12 @@
 import React, { Component } from "react";
 import "./Project.css";
-<<<<<<< HEAD
-=======
 import {withRouter} from 'react-router';
-import Img from 'react-image';
 import {Carousel} from 'react-bootstrap';
-import pic11 from "../../../assets/Projects/DIA-BEACON/pic1.jpg";
-import pic12 from "../../../assets/Projects/DIA-BEACON/pic2.jpg";
-import pic13 from "../../../assets/Projects/DIA-BEACON/pic3.jpg";
->>>>>>> parent of 8c1c572... nothing
 import Header from "../../Shared/Header/Header";
 
 
 class Project extends Component {
     render() {
-<<<<<<< HEAD
-=======
-        const slideItems = pics.map((pic, index) =>
-            <Carousel.Item className="photo">
-                <Img alt="slide" key={index} src={pic} />
-            </Carousel.Item>
-        );
->>>>>>> parent of 8c1c572... nothing
         return(
             <div>
             <Header title="PROJECT"/>
