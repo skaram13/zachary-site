e449d331fb67e1055176cdec82a7d06bc6e3d012
skaram13 <skaram13st@gmail.com>
2018-07-18 21:47:36 -0400

    added linkedin links and pics

--- front-end/src/Main/CV/CV.js
+++ front-end/src/Main/CV/CV.js
@@ -3,6 +3,8 @@ import "./CV.css";
 import Header from "../Shared/Header/Header";
 import resume from "../../assets/TORRES_RESUME 2.5.pdf";
 import email_logo from "../../assets/black-email-envelope.jpg";
+import linkedin from "../../assets/linkedin.jpg";
+
 import Footer from "../Shared/Footer/Footer";
 
 
@@ -16,12 +18,18 @@ class CV extends Component {
                         <h1><a href={resume}>LINK TO CURRICULUM VITAE</a></h1>
                     </div>
                     <div className="row">
-                        <h3>GET IN TOUCH</h3>
-                    </div>
-                    <div className="row">
                         <h4>
-                            <img className="icon" src={email_logo} alt={"email"}/>
-                            zacharygtorres@gmail.com
+
+                            <a href="mailto:zacharygtorres@gmail.com">
+                                <img className="icon" src={email_logo} alt={"email"}/>
+                                zacharygtorres@gmail.com
+                            </a>
+                        </h4>
+                        <h4>
+                            <a href="https://www.linkedin.com/in/zachary-torres-20a039a6">
+                                <img className="icon" src={linkedin} alt={"email"}/>
+                                Connect on LinkedIn
+                            </a>
                         </h4>
                     </div>
                 </div>
